3c3b009996333e729b1e39fc54fdabce9841b265
a840817a <a840817a@yahoo.com.tw>
2023-06-11 02:14:23 +0800

    fix entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ nginx -g "daemon off;" &
 
 # 抓取 m3u8 串流並轉換為 HLS
 ffmpeg -i "$HLS_URL" \
-    -c:v copy -c:a copy -f hls -ignore_io_errors 1 -hls_playlist_type event /tmp/hls/output.m3u8 \
+    -c:v copy -c:a copy -f hls -ignore_io_errors 1 -hls_playlist_type event /tmp/hls/output.m3u8
 
 ffmpeg -i "/tmp/hls/output.m3u8" \
     -c:v copy -c:a copy /tmp/output.mp4
